076d128e39a83b9e36c3dfe413f9abfb144e0ad4
John P Quense <jpquense@gmail.com>
2018-03-10 17:17:15 -0500

    modal added to home.html

---
+++ public/goals.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const MOCK_GRATITUDES = {
+	"gratitudes": [
+        {
+            "id": "1111111",
+            "item1": "I am grateful for my friend taking me out to lunch.",
+            "item2": "I am grateful for my friend taking me out to lunch.",
+            "item3": "I am grateful for my friend taking me out to lunch.",
+            "date": "May 3, 2018",
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "2222222",
+            "item1": "Grateful for a new car!",
+            "item2": "Grateful for a new car!",
+            "item3": "Grateful for a new car!",
+            "date": "May 3, 2018",
+            "publishedAt": 1470012976609
+        },
+        {
+            "id": "333333",
+            "item1": "I am grateful for karate class.",
+            "item2": "I am grateful for karate class.",
+            "item3": "I am grateful for karate class.",
+            "date": "May 3, 2018",
+            "publishedAt": 1470011976609
+        },
+        {
+            "id": "4444444",
+            "item1": "I am grateful for thinkful",
+            "item2": "I am grateful for thinkful",
+            "item3": "I am grateful for thinkful",
+            "date": "May 3, 2018",
+            "publishedAt": 1470009976609
+        }
+    ]
+};
+
+const MOCK_GOALS = {
+	"goals": [
+        {
+            "id": "1111111",
+            "goal1": "I will go out to lunch with my friend.",
+            "goal2": "I will go out to lunch with my friend.",
+            "goal3": "I will go out to lunch with my friend.",
+            "date": "May 3, 2018",
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "2222222",
+            "goal1": "I will wash my car!",
+            "goal2": "I will wash my car!",
+            "goal3": "I will wash my car!",
+            "date": "May 3, 2018",
+            "publishedAt": 1470012976609
+        },
+        {
+            "id": "333333",
+            "goal1": "I am going to karate class.",
+            "goal2": "I am going to karate class.",
+            "goal3": "I am going to karate class.",
+            "date": "May 3, 2018",
+            "publishedAt": 1470011976609
+        },
+        {
+            "id": "4444444",
+            "goal1": "I will study my thinkful course work",
+            "goal2": "I will study my thinkful course work",
+            "goal3": "I will study my thinkful course work",
+            "date": "May 3, 2018",
+            "publishedAt": 1470009976609
+        }
+    ]
+};
+
+$(function() {
+    console.log('goals.js is attached and running!');
+})
--- public/home.js
+++ public/home.js
@@ -1,125 +1,85 @@
 'use strict';
 
-const MOCK_DATA = {
-	"gratitudeList": [
+const MOCK_GRATITUDES = {
+	"gratitudes": [
         {
             "id": "1111111",
-            "item1": "I am grateful for my friend taking me out to lunch.",
-            "item2": "I am grateful for my friend taking me out to lunch.",
-            "item3": "I am grateful for my friend taking me out to lunch.",
-            "date": "May 3, 2018",
+            "list": [
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch."
+            ],
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "22222222",
+            "list": [
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch."
+            ],
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "3333333",
+            "list": [
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch."
+            ],
+            "publishedAt": 1470016976609
+        },
+        {
+            "id": "4444444",
+            "list": [
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch.",
+                "I am grateful for my friend taking me out to lunch."
+            ],
+            "publishedAt": 1470016976609
+        },
+
+    ]
+};
+
+const MOCK_GOALS = {
+	"goals": [
+        {
+            "id": "1111111",
+            "goal-list": [
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend."
+            ],
             "publishedAt": 1470016976609
         },
         {
             "id": "2222222",
-            "item1": "Grateful for a new car!",
-            "item2": "Grateful for a new car!",
-            "item3": "Grateful for a new car!",
-            "date": "May 3, 2018",
-            "publishedAt": 1470012976609
-        },
-        {
-            "id": "333333",
-            "item1": "I am grateful for karate class.",
-            "item2": "I am grateful for karate class.",
-            "item3": "I am grateful for karate class.",
-            "date": "May 3, 2018",
-            "publishedAt": 1470011976609
-        },
-        {
-            "id": "4444444",
-            "item1": "I am grateful for thinkful",
-            "item2": "I am grateful for thinkful",
-            "item3": "I am grateful for thinkful",
-            "date": "May 3, 2018",
-            "publishedAt": 1470009976609
-        }
-    ]
-};
-
-const MOCK_DATA_GOALS = {
-    "goals": [
-        {
-            "id": "1111111",
-            "plan1": "I am going to work 5 hrs",
-            "plan2": "I am going to work 5 hrs",
-            "plan3": "I am going to work 5 hrs",
-            "date": "June 6, 2018",
+            "goal-list": [
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend."
+            ],
             "publishedAt": 1470016976609
         },
         {
-            "id": "2222222",
-            "plan1": "I am going to eat 65 hrs",
-            "plan2": "I am going to eat 65 hrs",
-            "plan3": "I am going to eat 65 hrs",
-            "date": "March 30, 2018",
-            "publishedAt": 1470012976609
-        },
-        {
-            "id": "333333",
-            "plan1": "I am going to walk 65 miles",
-            "plan2": "I am going to walk 65 miles",
-            "plan3": "I am going to walk 65 miles",
-            "date": "January 5, 2018",
-            "publishedAt": 1470011976609
-        },
-        {
-            "id": "4444444",
-            "plan1": "I am going to code all day and then walk 65 miles",
-            "plan2": "I am going to code all day and then walk 65 miles",
-            "plan3": "I am going to code all day and then walk 65 miles",
-            "date": "December 2, 2018",
-            "publishedAt": 1470009976609
-        }
-    ]
-};
-
-const MOCK_DATA_USERS = {
-    "users": [
-        {
-            "id": "1111111",
-           "name": {
-               "first-name": "John",
-               "last-name": "Smith"
-           },
-           "gender": "Male",
-            "bithday": "June 6, 2018",
-            "reason-joined": "I have such a hard time prioritizing my life",
-            "joined": 1470016976609
-        },
-        {
-            "id": "222222",
-           "name": {
-               "first-name": "John",
-               "last-name": "Smith"
-           },
-           "gender": "Male",
-            "bithday": "June 6, 2018",
-            "reason-joined": "I have such a hard time prioritizing my life",
-            "joined": 1470016976609
-        },
-        {
-            "id": "333333",
-           "name": {
-               "first-name": "John",
-               "last-name": "Smith"
-           },
-           "gender": "Male",
-            "bithday": "June 6, 2018",
-            "reason-joined": "I have such a hard time prioritizing my life",
-            "joined": 1470016976609
+            "id": "3333333",
+            "goal-list": [
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend."
+            ],
+            "publishedAt": 1470016976609
         },
         {
             "id": "444444",
-           "name": {
-               "first-name": "John",
-               "last-name": "Smith"
-           },
-           "gender": "Male",
-            "bithday": "June 6, 2018",
-            "reason-joined": "I have such a hard time prioritizing my life",
-            "joined": 1470016976609
-        }
+            "goal-list": [
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend.",
+                "I will go out to lunch with my friend."
+            ],
+            "publishedAt": 1470016976609
+        },
     ]
 };
 
@@ -129,93 +89,55 @@ const MOCK_DATA_USERS = {
 // timeout function that returns mock data, it will 
 // use jQuery's AJAX functionality to make a call 
 // to the server and then run the callbackFn
-function getGratitudeList(callbackFN) {
+function getGratitudes(callbackFN) {
     // we use a `setTimeout` to make this asynchronous
     // as it will be with a real ajax call.
-        setTimeout(function() { callbackFN(MOCK_DATA)}, 100);
+        setTimeout(function() { callbackFN(MOCK_GRATITUDES)}, 100);
 }
 
-// this function stays the same when we connect
-// to real API layer
-function displayGratitudeList(data) {
-    data.gratitudeList.forEach((list, index) => {
-        console.log(list, index);
-        $('body').append(`
-            <h2>Gratitude list from: ${list.date}</h2>
-            <ol>
-                <li>${list.item1}</li>
-                <li>${list.item2}</li>
-                <li>${list.item3}</li>
-            </ol>
-        `); 
-    });
+function displayGratitudes(data) {
+    let listLength = 0;
+    
+    data.gratitudes.forEach((obj, idx) => {
+        listLength += obj.list.length;
+    })
+    console.log(data.gratitudes, listLength);
+    // $('.js-modal-header').val('My Gratitudes');
+    // $('.js-list-length').val(`I was grateful ${listLength} times!`);
+    // // $('.js-modal-btn-max').attr(max, listLength);
+    $('.js-modal').html(`
+        <header>
+        <h2 class="js-modal-header">I am grateful for . . . </h2>
+        </header>
+        <div class="modal-body row">
+            <div class="count col-12">
+                <h3>Gratitude Count: ${listLength}</h3>
+            </div>
+            <form class="count">
+            <label class="col-4" for="gratitudes">How many gratitudes do you want to view?</label>
+            <input class="col-4" type="number" placeholder="1" id="gratitudes" min="1" max="${listLength}">
+            <button class="col-4 js-modal-btn-max" type="submit" ">Get Gratitudes</button>
+            </form>
+            <div class="js-grat-list">
+
+            </div>
+        </div>
+        <footer>
+            <button class="js-modal-btn-close">Close</button>
+        </footer>
+    `);
+    $('.modal').slideDown('fast');
 }
 
-// this function can stay the same even when we
-// are connection to real API
-function getAndDisplayGratitudeList() {
-	getGratitudeList(displayGratitudeList);
+function getAndDisplayGratitudes() {
+	getGratitudes(displayGratitudes);
 }
 
-function getGoalList(callbackFN) {
-    // we use a `setTimeout` to make this asynchronous
-    // as it will be with a real ajax call.
-        setTimeout(function() { callbackFN(MOCK_DATA)}, 100);
-}
 
-// this function stays the same when we connect
-// to real API layer
-function displayGoalList(data) {
-    data.goalList.forEach((list, index) => {
-        console.log(list, index);
-        $('body').append(`
-            <h2>Goal list from: ${list.date}</h2>
-            <ol>
-                <li>${list.plan1}</li>
-                <li>${list.plan2}</li>
-                <li>${list.plan3}</li>
-            </ol>
-        `); 
-    });
-}
+$('.js-gratitude-btn').click(function() {
+    getAndDisplayGratitudes();
+}); 
 
-// this function can stay the same even when we
-// are connection to real API
-function getAndDisplayGoalList() {
-	getGoalList(displayGoalList);
-}
-
-function getLoveNotes(callbackFN) {
-    // we use a `setTimeout` to make this asynchronous
-    // as it will be with a real ajax call.
-        setTimeout(function() { callbackFN(MOCK_DATA)}, 100);
-}
-
-// this function stays the same when we connect
-// to real API layer
-function displayLoveNotes(data) {
-    data.loveNotes.forEach((list, index) => {
-        console.log(list, index);
-        $('body').append(`
-            <h2>Love Notes from: ${list.date}</h2>
-            <ol>
-                <li>${list.note1}</li>
-                <li>${list.note2}</li>
-                <li>${list.note3}</li>
-            </ol>
-        `); 
-    });
-}
-
-// this function can stay the same even when we
-// are connection to real API
-function getAndDisplayLoveNotes() {
-	getLoveNotes(displayLoveNotes);
-}
-
-//  on page load do this
-$(function() {
-    getAndDisplayGratitudeList();
-    getAndDisplayGoalList();
-    getAndDisplayLoveNotes();
-})
+$('.modal').on('click', '.js-modal-btn-close', function() {
+    $('.modal').slideUp('fast');
+}); 
